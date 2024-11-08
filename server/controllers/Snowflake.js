604335ded162953d006eb9060d3bc61fc031be1c
TheMostOP <lcs3087@rit.edu>
2024-11-07 22:19:51 -0500

    Update Snowflake.js

--- server/controllers/Snowflake.js
+++ server/controllers/Snowflake.js
@@ -2,7 +2,7 @@ const models = require('../models');
 
 const { Domo } = models;
 
-const snowflakePage = async (req, res) => res.render('snowflake');
+const snowflakePage = async (req, res) => res.render('app');
 
 const playSnowflake = async (req, res) => {
   try {
