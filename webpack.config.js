d2db07dc2caaf679a6a9acd8b6d2781e86f4079a
TheMostOP <lcs3087@rit.edu>
2024-11-07 21:54:42 -0500

    Fixed issue with snowflake page display

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const path = require('path');
 module.exports = {
     entry: {
         app: './client/maker.jsx',
+        snowflakeApp: './client/snowflake.jsx',
         login: './client/login.jsx',
     },
     module: {
